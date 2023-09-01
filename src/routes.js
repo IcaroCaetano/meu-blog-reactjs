05e0408e11e42eb85aa518654ecf3bf58ad3a440
IcaroCaetano <icaro.caetano@gmail.com>
2023-09-01 16:47:13 -0300

    feat: Inclusao de rotas para as postagens

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu"
 import Rodape from "./componentes/Rodape";
 import PaginaPadrao from "./componentes/PaginaPadrao";
+import Post from "./paginas/Post";
 
 //const pagina = window.location.pathname === '/' ? <Inicio/> : <SobreMim/>
 
@@ -14,8 +15,9 @@ function AppRoutes() {
       <Menu/>
       <Routes>
         <Route path= "/" element={<PaginaPadrao />}>
-            <Route path= "/" element={<Inicio />} />
-            <Route path= "/sobremim" element={<SobreMim />} />
+            <Route index element={<Inicio />} />
+            <Route path= "sobremim" element={<SobreMim />} />
+            <Route path="posts/:id" element={<Post/>}></Route>
         </Route>
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
