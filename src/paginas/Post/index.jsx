a227fbaa9ec6b78ce4e90d80d5f2475815d33e7e
IcaroCaetano <icaro.caetano@gmail.com>
2023-09-04 14:23:28 -0300

    refactor: ajustes

--- src/paginas/Post/index.jsx
+++ src/paginas/Post/index.jsx
@@ -3,32 +3,30 @@ import { useParams } from 'react-router-dom';
 import posts from './../../json/posts.json';
 import PostModelo from './../../componentes/PostModelo';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-import styles from './Post.css';
+import './Post.css';
 
 export default function Post() {
-
   const parametros = useParams();
 
   const post = posts.find((post) => {
-    return post.id === Number(parametros.id);
-  });
+      return post.id === Number(parametros.id);
+  })
 
   if (!post) {
-    return <h1>Post não encontado...</h1>
+      return <h1>Post não encontrado...</h1>
   }
 
-  console.log(post);
-
   return (
-    <PostModelo  
-        fotoCapa={`./assets/posts/${post.id}/capa.png`}
-        titulo={post.titulo}
-        >
-          <div className="post-markdown-container"> 
-            <ReactMarkdown>
-              {post.texto}
-            </ReactMarkdown>
+      <PostModelo
+          fotoCapa={`/assets/posts/${post.id}/capa.png`}
+          titulo={post.titulo}
+      >
+          <div className="post-markdown-container">
+              <ReactMarkdown>
+                  {post.texto}
+              </ReactMarkdown>
           </div>
-    </PostModelo>
+          
+      </PostModelo>
   )
 }
