fcf1581d7992f39767344229aedfeac7690f38ea
IcaroCaetano <icaro.caetano@gmail.com>
2023-09-11 15:21:45 -0300

    refactor: ajuste cards outros posts

--- src/componentes/PostCard/index.jsx
+++ src/componentes/PostCard/index.jsx
@@ -5,20 +5,18 @@ import BotaoPrincipal from '../BotaoPrincipal';
 
 export default function PostCard({ post }) {
   return (
-    <Link to={`/posts/${post.id}`}>
-      <div className={styles.post}>
-          <img className={styles.capa}
-              src={`./assets/posts/${post.id}/capa.png`}
-              alt="Imagem de Capa do post"
-          />
-          <h2 className={styles.titulo}>
-            {post.titulo}
-          </h2>
-          <BotaoPrincipal>
-            Ler
-          </BotaoPrincipal>
-      </div>
-    </Link>
+      <Link to={`/posts/${post.id}`}>
+          <div className={styles.post}>
+              <img
+                  className={styles.capa}
+                  src={`/assets/posts/${post.id}/capa.png`}
+                  alt="Imagem de capa do post"
+              />
 
+              <h2 className={styles.titulo}>{post.titulo}</h2>
+
+              <BotaoPrincipal>Ler</BotaoPrincipal>
+          </div>
+      </Link>
   )
 }
